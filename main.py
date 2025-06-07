16abaeffb2f6a4ea5699c6de3804888835b19dee
Aryan <aryanthegreat05@gmail.com>
2025-06-07 19:54:50 +0530

    main.py updated frontend website in allow_origins

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://text-summariser-frontend.vercel.app/"],
+    allow_origins=["https://text-summariser-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
